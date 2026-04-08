5dfdd213523f93e301d2d60975e4ee6ba97e1fd0
Sean Wall <seancwall@mac.com>
2026-04-08 17:56:27 -0400

    Changes through section 3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod color;
+mod vec3;
+
+use std::io;
+
+use color::Color;
+
 fn main() {
     // Image
 
@@ -15,11 +22,8 @@ fn main() {
             let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
             let b = 0.25;
 
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-
-            print!("{} {} {}\n", ir, ig, ib);
+            let pixel_color = Color::new(r, g, b);
+            color::write_color(&mut io::stdout(), pixel_color);
         }
     }
 

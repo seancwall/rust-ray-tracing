52cb7a2b7f06df0620a36e388f49a66942523364
Sean Wall <seancwall@mac.com>
2026-04-09 18:00:08 -0400

    Changes through section 7

--- src/common.rs
+++ src/common.rs
@@ -1,4 +1,5 @@
 // Constants
+use rand::Rng;
 
 pub use std::f64::consts::PI;
 pub use std::f64::INFINITY;
@@ -7,4 +8,21 @@ pub use std::f64::INFINITY;
 
 pub fn degrees_to_radians(degrees: f64) -> f64 {
   degrees * PI / 180.0
+}
+
+pub fn random_double() -> f64 {
+  rand::thread_rng().r#gen()
+}
+
+pub fn random_double_range(min: f64, max: f64) -> f64 {
+  min + (max - min) * random_double()
+}
+
+pub fn clamp(x: f64, min: f64, max: f64) -> f64 {
+  if x < min {
+    return min;
+  } else if x > max {
+    return max;
+  }
+  x
 }

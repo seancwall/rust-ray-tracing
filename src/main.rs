d55779fe6f812f1cf9a7a0c442fa32d35527ea9d
Sean Wall <seancwall@mac.com>
2026-04-13 09:05:04 -0400

    Lower image size and samples per pixel to more reasonable values

--- src/main.rs
+++ src/main.rs
@@ -116,9 +116,9 @@ fn main() {
     // Image
 
     const ASPECT_RATIO: f64 = 3.0 / 2.0;
-    const IMAGE_WIDTH: i32 = 1200;
+    const IMAGE_WIDTH: i32 = 800;
     const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
-    const SAMPLES_PER_PIXEL: i32 = 500;
+    const SAMPLES_PER_PIXEL: i32 = 100;
     const MAX_DEPTH: i32 = 50;
 
     // World

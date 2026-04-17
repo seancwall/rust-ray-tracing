8664deee2986182000275af2f0b23f2a0bfdbf96
Sean Wall <sean.wall@baesystems.us>
2026-04-17 09:45:24 -0400

    Use high quality settings

--- src/main.rs
+++ src/main.rs
@@ -109,10 +109,10 @@ fn main() {
     
     // Image
 
-    const ASPECT_RATIO: f64 = 3.0 / 2.0;
-    const IMAGE_WIDTH: i32 = 800;
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: i32 = 1920;
     const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
-    const SAMPLES_PER_PIXEL: i32 = 100;
+    const SAMPLES_PER_PIXEL: i32 = 500;
     const MAX_DEPTH: i32 = 50;
     
     // World
